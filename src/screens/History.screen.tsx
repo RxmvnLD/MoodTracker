13891b6b40cc0b9cb9a93745ff8df6425b3f08ee
RxmvnLD <65473002+RxmvnLD@users.noreply.github.com>
2023-10-05 15:01:56 -0600

    Bottom navigation

---
+++ src/screens/History.screen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const History: React.FC = () => {
+  return (
+    <View>
+      <Text>History.screen</Text>
+    </View>
+  );
+};
+
+export default History;
---
+++ src/screens/Home.screen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home.screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
